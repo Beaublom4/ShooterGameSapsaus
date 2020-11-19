23acdb6a6c74f14ed94da7ffa4bccb5a1ddd6749
KoningSherwood <ramonzwiep@hotmail.com>
2020-11-19 14:33:02 +0100

    Added Pistol Script

---
+++ SapsausShooter/Assets/Ramon/Pistol.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    public Gun weapon;
+    public Camera fpsCam;
+
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / weapon.fireRate;
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        weapon.muzzleFlash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, weapon.range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                hit.transform.GetComponent<BodyHit>().HitPart(weapon);
+            }
+
+            GameObject impactGO = Instantiate(weapon.impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+}
