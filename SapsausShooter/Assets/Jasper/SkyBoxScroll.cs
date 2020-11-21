b91333f1cf9eb0936302c918911974636444c966
Jasper0076 <60340192+Jasper0076@users.noreply.github.com>
2020-11-21 18:13:36 +0100

    SkyboxScroller

---
+++ SapsausShooter/Assets/Jasper/SkyBoxScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxScroll : MonoBehaviour
+{
+    public Material skyBoxShaderMaterial;
+    public float woosh,wooshKeer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Invoke("scroll", (Mathf.Lerp(minimum, maximum, t));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        woosh += Time.time *wooshKeer;
+        skyBoxShaderMaterial.SetFloat("Vector1_B483EFBD", woosh);
+
+        //"Vector1_B483EFBD"
+    }
+}
