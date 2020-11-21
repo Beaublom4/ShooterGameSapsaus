62ea49b2992d430dcdd65d6bc6631bb53ffce516
Jasper0076 <60340192+Jasper0076@users.noreply.github.com>
2020-11-21 17:44:50 +0100

    DissolveShader

---
+++ SapsausShooter/Assets/Jasper/Dissolve.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    public Material dissolveMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Dead()
+    {
+        dissolveMat.SetFloat("Vector1_4FF20CCE", 1);
+    }
+}
